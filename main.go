14a137d74466bcb761834510e93eb1ec90103810
thanhvodev <nthanhatee@gmail.com>
2025-12-09 15:43:43 +0700

    echo http request

--- main.go
+++ main.go
@@ -1,7 +1,80 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"go-crypto-exchange/orderbook"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	e := echo.New()
+
+	exchange := NewExchange()
+
+	e.POST("/order", exchange.handlePlaceOrder)
+
+	e.Start(":3000")
+
+}
+
+type Market string
+
+const (
+	MarketETH Market = "ETH"
+)
+
+type OrderType string
+
+const (
+	OrderTypeLimit  OrderType = "LIMIT"
+	OrderTypeMarket OrderType = "MARKET"
+)
+
+type Exchange struct {
+	orderbooks map[Market]*orderbook.OrderBook
+}
+
+func NewExchange() *Exchange {
+
+	orderbooks := make(map[Market]*orderbook.OrderBook)
+	orderbooks[MarketETH] = orderbook.NewOrderBook()
+
+	return &Exchange{
+		orderbooks: orderbooks,
+	}
+}
+
+type PlaceOrderRequest struct {
+	Type   OrderType
+	Market Market
+	Bid    bool
+	Size   float64
+	Price  float64
+}
+
+func (ex *Exchange) handlePlaceOrder(c echo.Context) error {
+
+	var placeOrderData PlaceOrderRequest
+	if err := json.NewDecoder(c.Request().Body).Decode(&placeOrderData); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
+	}
+
+	market := Market(placeOrderData.Market)
+	ob := ex.orderbooks[market]
+
+	requestOrderType := OrderType(placeOrderData.Type)
+
+	order := orderbook.NewOrder(placeOrderData.Bid, placeOrderData.Size)
+
+	if requestOrderType == OrderTypeLimit {
+		ob.PlaceLimitOrder(placeOrderData.Price, order)
+	} else if requestOrderType == OrderTypeMarket {
+		ob.PlaceMarketOrder(order)
+	} else {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "invalid order type"})
+	}
+
+	return c.JSON(http.StatusOK, map[string]string{"message": "Order placed"})
 }
---
+++ orderbook/orderbook_test.go
@@ -0,0 +1,117 @@
+package orderbook
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func assert(t *testing.T, a, b any) {
+	if !reflect.DeepEqual(a, b) {
+		t.Errorf("Expected %v, got %v", a, b)
+	}
+}
+
+func TestLimit(t *testing.T) {
+	l := NewLimit(10_000)
+	orderA := NewOrder(true, 10)
+	orderB := NewOrder(true, 20)
+	orderC := NewOrder(false, 30)
+
+	l.addOrder(orderA)
+	l.addOrder(orderB)
+	l.addOrder(orderC)
+
+	l.deleteOrder(orderB)
+
+	fmt.Println(l)
+}
+
+func TestPlaceLimitOrder(t *testing.T) {
+	ob := NewOrderBook()
+
+	buyOrderA := NewOrder(true, 100)
+	ob.PlaceLimitOrder(10_000, buyOrderA)
+
+	buyOrderB := NewOrder(true, 200)
+	ob.PlaceLimitOrder(10_000, buyOrderB)
+
+	assert(t, len(ob.Bids()), 1)
+}
+
+func TestPlaceMarketOrder(t *testing.T) {
+
+	ob := NewOrderBook()
+
+	buyOrderA := NewOrder(true, 100)
+	ob.PlaceLimitOrder(10_000, buyOrderA)
+	sellOrderA := NewOrder(false, 50.0)
+	matches := ob.PlaceMarketOrder(sellOrderA)
+	assert(t, len(matches), 1)
+	assert(t, matches[0].Bid.Size, 50.0)
+	assert(t, ob.BidLimits[10_000].TotalVolume, 50.0)
+
+}
+
+func TestPlaceMarketOrderMultiFill(t *testing.T) {
+	ob := NewOrderBook()
+
+	buyOrderA := NewOrder(true, 10.0)
+	buyOrderB := NewOrder(true, 15.0)
+	buyOrderC := NewOrder(true, 20.0)
+	buyOrderD := NewOrder(true, 1.0)
+
+	// place limit orders
+	ob.PlaceLimitOrder(10_000, buyOrderA)
+	ob.PlaceLimitOrder(10_000, buyOrderD)
+	ob.PlaceLimitOrder(11_000, buyOrderB)
+	ob.PlaceLimitOrder(12_000, buyOrderC)
+
+	assert(t, ob.BidTotalVolume(), 46.0)
+
+	// place market order
+	sellOrderA := NewOrder(false, 34.0)
+	matchesA := ob.PlaceMarketOrder(sellOrderA)
+	assert(t, len(matchesA), 2)
+
+	sellOrderB := NewOrder(false, 2.0)
+	matchesB := ob.PlaceMarketOrder(sellOrderB)
+	assert(t, len(matchesB), 2)
+
+}
+
+func TestPlaceMarketOrderFillLimit(t *testing.T) {
+
+	ob := NewOrderBook()
+
+	buyOrderA := NewOrder(true, 15.0)
+	buyOrderB := NewOrder(true, 10.0)
+	buyOrderC := NewOrder(true, 20.0)
+
+	ob.PlaceLimitOrder(10_000, buyOrderA)
+	ob.PlaceLimitOrder(10_000, buyOrderB)
+	ob.PlaceLimitOrder(11_000, buyOrderC)
+
+	assert(t, ob.BidTotalVolume(), 45.0)
+
+	sellOrderA := NewOrder(false, 34.0)
+	matchesA := ob.PlaceMarketOrder(sellOrderA)
+
+	assert(t, len(matchesA), 2)
+
+	assert(t, len(ob.Bids()), 1)
+
+}
+
+func TestCancelOrder(t *testing.T) {
+	ob := NewOrderBook()
+
+	buyOrderA := NewOrder(true, 10.0)
+	buyOrderB := NewOrder(true, 15.0)
+	ob.PlaceLimitOrder(10_000, buyOrderA)
+	ob.PlaceLimitOrder(11_000, buyOrderB)
+	assert(t, len(ob.Bids()), 2)
+
+	ob.CancelOrder(buyOrderA)
+	assert(t, len(ob.Bids()), 1)
+}
